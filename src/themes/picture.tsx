a165e40c3a8f2da3a9f6d7846c2877c83d997bba
Lukas Bach <contact@lukasbach.com>
2025-07-19 15:28:18 +0200

    fix: picture template

--- src/themes/picture.tsx
+++ src/themes/picture.tsx
@@ -1,50 +1,53 @@
-import "../module-globals.js";
 import React from "react";
+import type { basicsItemsComponent } from "../components/atoms/basics-items-component.js";
+import "../module-globals.js";
 
 const basics = defineComponent({
   name: "basics",
-  schema: z.object({
-    basics: z.object({ name: z.string() }),
-    title: z
-      .object({
-        items: z.string().array().optional(),
-        image: z.string(),
-        rounded: z.boolean().optional(),
-        summary: z.string().optional(),
-      })
-      .optional(),
-  }),
+  schema: z.intersection(
+    defaultComponents.basics.schema,
+    z.object({
+      basics: z
+        .object({
+          image: z.string(),
+          roundedImage: z.boolean().optional(),
+        })
+        .optional(),
+    }),
+  ),
   component: ({ spec, styles, getComponent, resolvePath }) => {
     const Markdown = getComponent({ name: "markdown" });
+    const BasicsItems = getComponent({
+      name: "basicsItems",
+    } as typeof basicsItemsComponent);
     return (
-      spec.title && (
+      spec.basics && (
         <>
           <ReactPdf.View style={styles.container}>
             <ReactPdf.View style={styles.leftContainer}>
               <ReactPdf.Image
-                src={resolvePath(spec.title.image)}
+                src={resolvePath(spec.basics.image)}
                 style={[
                   styles.picture,
-                  spec.title.rounded ? { borderRadius: "9999pt" } : {},
+                  spec.basics.roundedImage ? { borderRadius: "9999pt" } : {},
                 ]}
               />
             </ReactPdf.View>
             <ReactPdf.View style={styles.rightContainer}>
               <Markdown style={styles.name}>{spec.basics.name}</Markdown>
               <ReactPdf.View style={styles.itemContainer}>
-                {spec.title.items?.map((item, index) => (
-                  <Markdown
-                    key={index}
-                    style={[styles.item, index === 0 ? styles.firstItem : {}]}
-                  >
-                    {item}
-                  </Markdown>
-                ))}
+                <BasicsItems
+                  renderItem={(item, key) => (
+                    <ReactPdf.View style={styles.item} key={key}>
+                      <Markdown>{item}</Markdown>
+                    </ReactPdf.View>
+                  )}
+                />
               </ReactPdf.View>
             </ReactPdf.View>
           </ReactPdf.View>
-          {spec.title.summary && (
-            <Markdown style={styles.summary}>{spec.title.summary}</Markdown>
+          {spec.basics.summary && (
+            <Markdown style={styles.summary}>{spec.basics.summary}</Markdown>
           )}
         </>
       )
@@ -74,11 +77,10 @@ const basics = defineComponent({
       display: "flex",
       flexDirection: "row",
       flexWrap: "wrap",
+      rowGap: "4pt",
+      columnGap: "12pt",
     },
     item: {
-      borderLeft: "1pt solid black",
-      paddingLeft: "5pt",
-      marginLeft: "5pt",
       color: "black",
     },
     firstItem: {
